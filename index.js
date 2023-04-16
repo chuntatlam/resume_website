875f6850bf05399c48c616d3b7d6045e440b1ad1
chuntatlam <lamct1996@gmail.com>
2023-04-16 21:13:18 +0800

    fix: dark mode not persisting across pages

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-function toggleDarkMode() {
-  var element = document.body;
-  element.classList.toggle("dark-mode");
+const toggleBtn = document.getElementById("dark-mode-btn");
+const element = document.body;
+let darkMode = localStorage.getItem("dark-mode");
+
+const enableDarkMode = () => {
+  element.classList.add("dark-mode");
+  localStorage.setItem("dark-mode", "enabled");
 }
 
+const disableDarkMode = () => {
+  element.classList.remove("dark-mode");
+  localStorage.setItem("dark-mode", "disabled");
+}
+
+if (darkMode === "enabled") {
+  enableDarkMode();
+}
+
+toggleBtn.addEventListener("click", (e) => {
+  darkMode = localStorage.getItem("dark-mode");
+  if (darkMode === "disabled") {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  }
+});
